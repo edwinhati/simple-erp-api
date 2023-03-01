f86dfc8e0c6800e88da249573b8178ea7c984e58
edwinhati <edwinhati@yahoo.com>
2023-03-01 16:03:33 +0800

    Change on product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -3,7 +3,7 @@ import {
   Get,
   Post,
   Body,
-  Patch,
+  Put,
   Param,
   Delete,
 } from '@nestjs/common';
@@ -30,7 +30,7 @@ export class ProductController {
     return await this.productService.findOne(id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   async update(
     @Param('id') id: string,
     @Body() updateProductDto: UpdateProductDto,
