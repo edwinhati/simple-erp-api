b4df4505bfa2d49e2ca5940640ced4c3d04c36b0
edwinhati <edwinhati@yahoo.com>
2023-03-01 12:58:25 +0800

    Change on product

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { Product, ProductDocument } from './entities/product.entity';
+import { Product, ProductDocument } from './schemas/product.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
